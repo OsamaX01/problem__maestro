35cc944829c8ecca2f8100cedf298c3804a126d6
osamax01 <shkukanios@gmail.com>
2024-04-28 11:46:57 +0300

    dasbourd app: add custom courses to each user

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from course.models import Course
 
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return render(request, "dashboard/guest.html")
     
-    return render(request, "dashboard/user_dashboard.html")
+    student_courses = Course.objects.filter(students=request.user)
+    instructor_courses = Course.objects.filter(instructor_id=request.user)
+    return render(request, "dashboard/user_dashboard.html", {
+        "courses" : (student_courses | instructor_courses).distinct()
+    })
