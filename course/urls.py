c6b8532568b144e7ccba102b9be4543a8a066df5
osamax01 <shkukanios@gmail.com>
2024-04-28 12:03:30 +0300

    course & dashboard apps: add course view and link it to the dashboard

--- course/urls.py
+++ course/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from . import views
+
+app_name = 'course'
 urlpatterns = [
-    
+    path("<int:course_id>", views.index, name='index')
 ]
--- course/views.py
+++ course/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Course
+
 # Create your views here.
+def index(request, course_id):
+    return render(request, "course/index.html", {
+        "course" : Course.objects.get(pk = course_id)
+    })
