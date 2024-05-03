5581f1168d8516927a006282601e3e052c09709a
osamax01 <shkukanios@gmail.com>
2024-05-03 17:05:40 +0300

    problem app: add problem view

--- problem/urls.py
+++ problem/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'problem'
 urlpatterns = [
-
+    path("<int:problem_id>", views.index, name='index'),
 ]
--- problem/views.py
+++ problem/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .models import Problem
+
 # Create your views here.
+def index(request, problem_id):
+    problem = Problem.objects.get(pk = problem_id)
+    samples = problem.testcases.filter(is_visable=True)
+
+    return render(request, "problem/index.html", {
+        "problem" : problem,
+        "samples" : samples, 
+    })
