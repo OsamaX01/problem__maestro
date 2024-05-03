9744d118dd0568063272a8fc19d973144ea1b133
osamax01 <shkukanios@gmail.com>
2024-05-03 16:19:31 +0300

    course app: connect problems to courses

--- course/models.py
+++ course/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from users.models import CustomUser
+from problem.models import Problem
 
 # Create your models here.
 class Course(models.Model):
@@ -8,7 +9,7 @@ class Course(models.Model):
     description = models.TextField(blank=True)
     instructor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="my_courses", limit_choices_to={'role': 'instructor'})
     students = models.ManyToManyField(CustomUser, blank=True, related_name="courses", limit_choices_to={'role': 'student'})
-    # TODO problems
+    problems = models.ManyToManyField(Problem, blank=True, related_name="courses")
 
     def __str__(self):
         return f"{self.name} course"
