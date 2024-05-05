e5b6bd314502f4a4662d5eae0c825b8b257382a6
osamax01 <shkukanios@gmail.com>
2024-05-05 22:32:28 +0300

    course app: add add_problem delete_problem from app inside each course for the instructor

--- course/urls.py
+++ course/urls.py
@@ -6,4 +6,6 @@ app_name = 'course'
 urlpatterns = [
     path("<int:course_id>", views.index, name='index'),
     path("create", views.create_course, name='create'),
+    path("add_problem_to_course/<int:course_id>", views.add_problem_to_course, name='add_problem_to_course'),
+    path("remove_problem_to_course/<int:course_id>", views.remove_problem_to_course, name='remove_problem_to_course'),
 ]
--- course/views.py
+++ course/views.py
@@ -3,11 +3,16 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Course
 from .forms import CourseForm
+from problem.models import Problem
 
 # Create your views here.
 def index(request, course_id):
+    course = Course.objects.get(pk = course_id)
+    unused_problems = Problem.objects.exclude(courses=course_id)
+
     return render(request, "course/index.html", {
-        "course" : Course.objects.get(pk = course_id)
+        "course" : course, 
+        "unused_problems" : unused_problems,
     })
 
 @login_required
@@ -29,3 +34,21 @@ def create_course(request):
     return render(request, 'course/create_course.html', {
         'form': form,
     })
+
+def add_problem_to_course(request, course_id):
+    if request.method == 'POST':
+        problem_id = request.POST.get('problem_id')
+        problem = Problem.objects.get(id = problem_id)
+        course = Course.objects.get(id = course_id)
+        course.problems.add(problem)
+        course.save()
+        return redirect('course:index', course_id=course_id)
+
+def remove_problem_to_course(request, course_id):
+    if request.method == 'POST':
+        problem_id = request.POST.get('problem_id')
+        problem = Problem.objects.get(id = problem_id)
+        course = Course.objects.get(id = course_id)
+        course.problems.remove(problem)
+        course.save()
+        return redirect('course:index', course_id=course_id)
